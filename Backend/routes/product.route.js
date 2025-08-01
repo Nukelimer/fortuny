818c94020144ca499ac14ca67c8283d5d58d3d32
Nukelimer <codewithremi@gmail.com>
2025-08-01 19:47:02 +0100

    added routes for the api, created the API for register, login and logout.

---
+++ Backend/routes/product.route.js
@@ -0,0 +1,34 @@
+import {
+        createProduct,
+        getAllProduct,
+        deleteProduct,
+        getProduct,
+        rateProduct,
+        updateProduct
+} from "../controller/product.controller.js";
+
+
+import express from "express"
+const router = express.Router();
+
+// RATING PRODUCT ROUTE
+router.put("/rating/:productId", rateProduct)
+
+// GET ALL PRODUCTS
+router.get("/", getAllProduct)
+
+// GET ONE PRODUCTS
+router.get("/find/:id", getProduct)
+
+// CREATE PRODUCT
+router.post("/", createProduct)
+
+// UPDATE PRODUCT
+router.put("/:id", updateProduct)
+
+// DELETE PRODUCT
+router.delete("/:id", deleteProduct)
+
+
+
+export default router;
